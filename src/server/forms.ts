bff116d05a8edc18ff25832da1960dec2f72e113
Blessed Sibanda <blessed15896@gmail.com>
2024-07-20 18:22:20 +0200

    Use handlebars default data sanitization

--- src/server/forms.ts
+++ src/server/forms.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import type { Express } from "express";
 import multer from "multer";
-import { sanitizeValue } from "./sanitize";
 
 const fileMiddleware = multer({ storage: multer.memoryStorage() });
 
@@ -18,14 +17,9 @@ export const registerFormRoutes = (app: Express) => {
   });
 
   app.post("/form", fileMiddleware.single("datafile"), (req, res) => {
-    res.setHeader("Content-Type", "text/html");
-    for (const key in req.body) {
-      res.write(`<div>${key}: ${sanitizeValue(req.body[key])}</div>`);
-    }
-    if (req.file) {
-      res.write(`<div>File: ${req.file.originalname}</div>`);
-      res.write(`<div>${sanitizeValue(req.file.buffer.toString())}</div>`);
-    }
-    res.end();
+    res.render("formData", {
+      ...req.body,
+      fileData: req.file?.buffer.toString(),
+    });
   });
 };
