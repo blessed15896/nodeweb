c4b8e59a30868b19ef316c3d8c245860c4f3273e
Blessed Sibanda <blessed15896@gmail.com>
2024-07-20 16:16:24 +0200

    Rewrite the counter application using client-side HTML templates

--- src/client/counter_custom.js
+++ src/client/counter_custom.js
@@ -5,5 +5,5 @@ export const Counter = (context) => `
   <button class="btn btn-primary m-2" action="incrementCounter">
     Increment
   </button>
-  <div>${context.counter % 2 ? Odd(context) : Even(context)}</div>
+  <div>${context.counter % 2 !== 0 ? Odd(context) : Even(context)}</div>
 `;
