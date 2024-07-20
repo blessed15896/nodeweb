704d829b1c98f124b69e1bb4ad564bd89b4a2fc2
Blessed Sibanda <blessed15896@gmail.com>
2024-07-20 19:52:40 +0200

    Client-side validation

--- src/client/client.js
+++ src/client/client.js
@@ -1,1 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {});
+import { validate } from "./client_validation";
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("age_form").onsubmit = (ev) => {
+    const data = new FormData(ev.target);
+    const nameValid = validate("name", data).required().minLength(5);
+    const ageValid = validate("age", data).isInteger();
+    const allValid = [nameValid, ageValid]
+      .flatMap((v_result) =>
+        Object.entries(v_result.results).map(([test, valid]) => {
+          let id = `err_${v_result.propertyName}_${test}`;
+          const e = document.getElementById(id);
+          e.classList.add("bg-dark-subtle");
+          e.style.display = valid ? "none" : "block";
+          return valid;
+        })
+      )
+      .every((v) => v === true);
+    console.log("allValid -->", allValid);
+    if (!allValid) {
+      ev.preventDefault();
+    }
+  };
+});
