87e1b174b429ca85a8fbddfe5536824ea4434638
Blessed Sibanda <blessed15896@gmail.com>
2024-07-20 17:50:18 +0200

    Receive form data from POST request

--- src/server/forms.ts
+++ src/server/forms.ts
@@ -1,13 +1,22 @@
+import express from "express";
 import type { Express } from "express";
 
 export const registerFormMiddleware = (app: Express) => {
-  // no middleware yet
+  app.use(express.urlencoded({ extended: true }));
 };
 
 export const registerFormRoutes = (app: Express) => {
   app.get("/form", (req, res) => {
     for (const key in req.query) {
       res.write(`${key}: ${req.query[key]}\n`);
+    }
+    res.end();
+  });
+
+  app.post("/form", (req, res) => {
+    res.write(`Content-Type: ${req.headers["content-type"]}\n`);
+    for (const key in req.body) {
+      res.write(`${key}: ${req.body[key]}\n`);
     }
     res.end();
   });
