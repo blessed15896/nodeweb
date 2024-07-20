1bd6ae953c0c66d45dd6d48f7211d074d6da1b6a
Blessed Sibanda <blessed15896@gmail.com>
2024-07-20 19:22:57 +0200

    Validate form data

--- src/server/forms.ts
+++ src/server/forms.ts
@@ -1,25 +1,31 @@
 import express from "express";
 import type { Express } from "express";
-import multer from "multer";
-
-const fileMiddleware = multer({ storage: multer.memoryStorage() });
+import { getValidationResults, validate } from "./validation";
 
 export const registerFormMiddleware = (app: Express) => {
   app.use(express.urlencoded({ extended: true }));
 };
 
 export const registerFormRoutes = (app: Express) => {
-  app.get("/form", (req, res) => {
-    for (const key in req.query) {
-      res.write(`${key}: ${req.query[key]}\n`);
-    }
-    res.end();
-  });
+  app.get("/form", (req, res) => res.render("age", { helpers: { pass } }));
 
-  app.post("/form", fileMiddleware.single("datafile"), (req, res) => {
-    res.render("formData", {
-      ...req.body,
-      fileData: req.file?.buffer.toString(),
-    });
-  });
+  app.post(
+    "/form",
+    validate("name").required().minLength(5),
+    validate("age").required().isInteger(),
+    (req, res) => {
+      const validation = getValidationResults(req);
+      const context = { ...req.body, validation, helpers: { pass } };
+      debugger;
+      if (validation.valid) {
+        context.nextage = Number.parseInt(req.body.age) + 1;
+      }
+      res.render("age", context);
+    }
+  );
+};
+
+const pass = (validation: any, propname: string, test: string) => {
+  let propResult = validation?.results?.[propname];
+  return `display:${!propResult || propResult[test] ? "none" : "block"}`;
 };
