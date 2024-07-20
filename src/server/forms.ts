e400d4878c6293955794923cd158195344a20ac7
Blessed Sibanda <blessed15896@gmail.com>
2024-07-20 18:03:57 +0200

    Receive uploaded files

--- src/server/forms.ts
+++ src/server/forms.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import type { Express } from "express";
+import multer from "multer";
+
+const fileMiddleware = multer({ storage: multer.memoryStorage() });
 
 export const registerFormMiddleware = (app: Express) => {
   app.use(express.urlencoded({ extended: true }));
@@ -13,10 +16,14 @@ export const registerFormRoutes = (app: Express) => {
     res.end();
   });
 
-  app.post("/form", (req, res) => {
+  app.post("/form", fileMiddleware.single("datafile"), (req, res) => {
     res.write(`Content-Type: ${req.headers["content-type"]}\n`);
     for (const key in req.body) {
       res.write(`${key}: ${req.body[key]}\n`);
+    }
+    if (req.file) {
+      res.write(`---\nFile: ${req.file.originalname}\n`);
+      res.write(req.file.buffer.toString());
     }
     res.end();
   });
