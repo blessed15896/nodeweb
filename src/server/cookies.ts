c69bc214382d037bdaee0084d16b441dd3618eb2
Blessed Sibanda <blessed15896@gmail.com>
2024-07-21 07:13:50 +0200

    Use signed cookies

--- src/server/cookies.ts
+++ src/server/cookies.ts
@@ -1,9 +1,14 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { signCookie, validateCookie } from "./cookies_signed";
 
 const setHeaderName = "Set-Cookie";
+const cookieSecret = "mysecret";
 
 export const setCookie = (res: ServerResponse, name: string, val: string) => {
-  let cookieVal: any[] = [`${name}=${val}; Max-Age=300; SameSite=Strict`];
+  const signedCookieVal = signCookie(val, cookieSecret);
+  let cookieVal: any[] = [
+    `${name}=${signedCookieVal}; Max-Age=300; SameSite=Strict`,
+  ];
   if (res.hasHeader(setHeaderName)) {
     cookieVal.push(res.getHeader(setHeaderName));
   }
@@ -23,7 +28,7 @@ export const getCookie = (
       const { name, val } = /^(?<name>.*)=(?<val>.*)$/.exec(cookie)
         ?.groups as any;
       if (name.trim() === key) {
-        result = val;
+        result = validateCookie(val, cookieSecret);
       }
     });
   });
