30431cf4ac8948825f62f61ea01c844e9c8a8a46
Blessed Sibanda <blessed15896@gmail.com>
2024-07-21 06:29:26 +0200

    Use sequelize orm

--- src/server/data/index.ts
+++ src/server/data/index.ts
@@ -1,6 +1,8 @@
+import { OrmRepository } from "./orm_repository";
 import { Repository } from "./repository";
 import { SqlRepository } from "./sqlRepository";
 
-const repository: Repository = new SqlRepository();
+// const repository: Repository = new SqlRepository();
+const repository: Repository = new OrmRepository();
 
 export default repository;
