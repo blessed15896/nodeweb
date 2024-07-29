c1be886e8eb02bbaa0891e883db4e71a3a975373
Blessed Sibanda <blessed15896@gmail.com>
2024-07-29 11:40:40 +0200

    Use feathersjs package for web service

--- src/server/api/http_adapter.ts
+++ src/server/api/http_adapter.ts
@@ -1,6 +1,3 @@
-import type { Express, Response } from "express";
-import { ValidationError } from "./validation_types";
-
 export interface WebService<T> {
   getOne(id: any): Promise<T | undefined>;
   getMany(query: any): Promise<T[]>;
@@ -8,73 +5,4 @@ export interface WebService<T> {
   delete(id: any): Promise<boolean>;
   replace(id: any, data: any): Promise<T | undefined>;
   modify(id: any, data: any): Promise<T | undefined>;
-}
-
-export function createAdapter<T>(
-  app: Express,
-  ws: WebService<T>,
-  baseUrl: string
-) {
-  app.get(baseUrl, async (req, res) => {
-    try {
-      res.json(await ws.getMany(req.query));
-      res.end();
-    } catch (err) {
-      writeResponse(err, res);
-    }
-  });
-
-  app.get(`${baseUrl}/:id`, async (req, res) => {
-    try {
-      const data = await ws.getOne(req.params.id);
-      if (data == undefined) res.writeHead(404);
-      else res.json(data);
-    } catch (err) {
-      writeResponse(err, res);
-    }
-    res.end();
-  });
-
-  app.post(baseUrl, async (req, res) => {
-    try {
-      const data = await ws.store(req.body);
-      res.json(data);
-      res.end();
-    } catch (err) {
-      writeResponse(err, res);
-    }
-  });
-
-  app.delete(`${baseUrl}/:id`, async (req, res) => {
-    try {
-      res.json(await ws.delete(req.params.id));
-      res.end();
-    } catch (err) {
-      writeResponse(err, res);
-    }
-  });
-
-  app.put(`${baseUrl}/:id`, async (req, res) => {
-    try {
-      res.json(await ws.replace(req.params.id, req.body));
-      res.end();
-    } catch (err) {
-      writeResponse(err, res);
-    }
-  });
-
-  app.patch(`${baseUrl}/:id`, async (req, res) => {
-    try {
-      res.json(await ws.modify(req.params.id, req.body));
-      res.json();
-    } catch (err) {
-      writeResponse(err, res);
-    }
-  });
-
-  const writeResponse = (err: any, res: Response) => {
-    console.error(err);
-    res.writeHead(err instanceof ValidationError ? 400 : 500);
-    res.end();
-  };
 }
--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,13 +1,29 @@
 import type { Express } from "express";
-import { createAdapter } from "./http_adapter";
 import { ResultWebService } from "./result_api";
 import { Validator } from "./validation_adapter";
 import { ResultWebServiceValidation } from "./result_api_validator";
+import feathersExpress, { rest } from "@feathersjs/express";
+import { feathers } from "@feathersjs/feathers";
+import { FeathersWrapper } from "./feathers_adapter";
+import { ValidationError } from "./validation_types";
 
 export const createApi = (app: Express) => {
-  createAdapter(
-    app,
-    new Validator(new ResultWebService(), ResultWebServiceValidation),
-    "/api/results"
+  const feathersApp = feathersExpress(feathers(), app).configure(rest());
+  const service = new Validator(
+    new ResultWebService(),
+    ResultWebServiceValidation
   );
+  feathersApp.use("/api/results", new FeathersWrapper(service));
+  feathersApp.hooks({
+    error: {
+      all: [
+        (ctx) => {
+          if (ctx.error instanceof ValidationError) {
+            ctx.http = { status: 400 };
+            ctx.error = undefined;
+          }
+        },
+      ],
+    },
+  });
 };
