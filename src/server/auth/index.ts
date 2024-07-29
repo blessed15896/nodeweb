a2b5225eca2929f50dc667e4e32205198937e70e
Blessed Sibanda <blessed15896@gmail.com>
2024-07-29 13:06:30 +0200

    Authenticate web service requests

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -1,6 +1,7 @@
 import type { Express } from "express";
 import { AuthStore } from "./auth_types";
 import { OrmAuthStore } from "./orm_authstore";
+import jwt from "jsonwebtoken";
 
 type User = { username: string };
 
@@ -19,6 +20,8 @@ declare global {
   }
 }
 
+const jwtSecret = "mytokensecret";
+
 const store: AuthStore = new OrmAuthStore();
 
 export const createAuth = (app: Express) => {
@@ -27,6 +30,16 @@ export const createAuth = (app: Express) => {
     if (username) {
       req.authenticated = true;
       req.user = { username };
+    } else if (req.headers.authorization) {
+      let token = req.headers.authorization;
+      if (token.startsWith("Bearer ")) token = token.substring(7);
+      try {
+        const decoded = jwt.verify(token, jwtSecret) as User;
+        req.authenticated = true;
+        req.user = { username: decoded.username };
+      } catch (err) {
+        // do nothing - cannot verify token
+      }
     } else req.authenticated = false;
     res.locals.user = req.user;
     res.locals.authenticated = req.authenticated;
@@ -54,6 +67,18 @@ export const createAuth = (app: Express) => {
       res.redirect(
         `/signin?username=${username}&password=${password}&failed=1`
       );
+  });
+
+  app.post("/api/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const result: any = {
+      success: await store.validateCredentials(username, password),
+    };
+    if (result.success)
+      result.token = jwt.sign({ username }, jwtSecret, { expiresIn: "1hr" });
+    res.json(result);
+    res.end();
   });
 
   app.post("/signout", async (req, res) =>
